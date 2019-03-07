5ff336d3204e209a1baa018351ce3c631090089a
Anthony D <dunahee@gmail.com>
2019-03-07 16:54:40 -0600

    database structure code and db name 'journey_tellr'

--- database.sql
+++ database.sql
@@ -1,5 +1,175 @@
+-- name your database "journey_tellr"
+
 CREATE TABLE "person" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial NOT NULL,
+	"email" varchar(40) NOT NULL UNIQUE,
+	"password" varchar(1000) NOT NULL,
+	"first_name" varchar(40) NOT NULL,
+	"last_name" varchar(40) NOT NULL,
+	"profile_pic" TEXT,
+	"date_created" DATE NOT NULL DEFAULT now(),
+	CONSTRAINT person_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "story" (
+	"id" serial NOT NULL,
+	"header_photo" varchar(240) NOT NULL,
+	"author" integer NOT NULL,
+	"title" varchar(80) NOT NULL UNIQUE,
+	"caption" varchar(280),
+	"intro" TEXT NOT NULL,
+	"date_started" DATE NOT NULL DEFAULT now(),
+	"completed" BOOLEAN NOT NULL DEFAULT 'false',
+	"last_edit" timestamp,
+	"is_template" BOOLEAN NOT NULL DEFAULT 'false',
+	CONSTRAINT story_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "chapter" (
+	"id" serial NOT NULL,
+	"story_id" integer,
+	"title" varchar(80) NOT NULL,
+	"text" TEXT,
+	"date_created" timestamp,
+	"order" integer NOT NULL,
+	CONSTRAINT chapter_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "team" (
+	"id" serial NOT NULL,
+	"name" varchar(80) NOT NULL UNIQUE,
+	"date_created" timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT team_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "contributor" (
+	"id" serial NOT NULL,
+	"person_id" integer NOT NULL,
+	"story_id" integer NOT NULL,
+	"status" varchar(10) NOT NULL,
+	CONSTRAINT contributor_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "photo" (
+	"id" serial NOT NULL,
+	"person_id" integer NOT NULL,
+	"photo" varchar(1000) UNIQUE,
+	"shareable" BOOLEAN NOT NULL DEFAULT 'false',
+	CONSTRAINT photo_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "story_likes" (
+	"id" serial NOT NULL,
+	"person_id" integer NOT NULL,
+	"story_id" integer NOT NULL,
+	CONSTRAINT story_likes_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "photo_caption" (
+	"id" serial NOT NULL,
+	"caption" varchar(280),
+	"chapter_id" integer NOT NULL,
+	"photo_id" integer NOT NULL,
+	"created_on" timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT photo_caption_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "template_story" (
+	"id" serial NOT NULL,
+	"creator" integer NOT NULL,
+	"name" varchar(280) NOT NULL UNIQUE,
+	"title" varchar(280) NOT NULL,
+	"caption" varchar(280) NOT NULL UNIQUE,
+	"placeholder_image" VARCHAR(280) NOT NULL UNIQUE,
+	"intro" TEXT NOT NULL,
+	"date_created" timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT template_story_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "team_person" (
+	"id" serial NOT NULL,
+	"team_id" integer NOT NULL,
+	"person_id" integer NOT NULL,
+	"status" varchar(10) NOT NULL,
+	CONSTRAINT team_person_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "template_chapter" (
+	"id" serial NOT NULL,
+	"template_id" integer NOT NULL,
+	"title" varchar(80) NOT NULL,
+	"text" TEXT NOT NULL,
+	"date_created" DATE NOT NULL DEFAULT now(),
+	"order" integer NOT NULL,
+	CONSTRAINT template_chapter_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "story" ADD CONSTRAINT "story_fk1" FOREIGN KEY ("author") REFERENCES "person"("id");
+
+ALTER TABLE "chapter" ADD CONSTRAINT "chapter_fk0" FOREIGN KEY ("story_id") REFERENCES "story"("id");
+
+
+ALTER TABLE "contributor" ADD CONSTRAINT "contributor_fk0" FOREIGN KEY ("person_id") REFERENCES "person"("id");
+ALTER TABLE "contributor" ADD CONSTRAINT "contributor_fk1" FOREIGN KEY ("story_id") REFERENCES "story"("id");
+
+ALTER TABLE "photo" ADD CONSTRAINT "photo_fk0" FOREIGN KEY ("person_id") REFERENCES "person"("id");
+
+ALTER TABLE "story_likes" ADD CONSTRAINT "story_likes_fk0" FOREIGN KEY ("person_id") REFERENCES "person"("id");
+ALTER TABLE "story_likes" ADD CONSTRAINT "story_likes_fk1" FOREIGN KEY ("story_id") REFERENCES "story"("id");
+
+ALTER TABLE "photo_caption" ADD CONSTRAINT "photo_caption_fk0" FOREIGN KEY ("chapter_id") REFERENCES "chapter"("id");
+ALTER TABLE "photo_caption" ADD CONSTRAINT "photo_caption_fk1" FOREIGN KEY ("photo_id") REFERENCES "photo"("id");
+
+ALTER TABLE "template_story" ADD CONSTRAINT "template_story_fk0" FOREIGN KEY ("creator") REFERENCES "person"("id");
+
+ALTER TABLE "team_person" ADD CONSTRAINT "team_person_fk0" FOREIGN KEY ("team_id") REFERENCES "team"("id");
+ALTER TABLE "team_person" ADD CONSTRAINT "team_person_fk1" FOREIGN KEY ("person_id") REFERENCES "person"("id");
+
+ALTER TABLE "template_chapter" ADD CONSTRAINT "template_chapter_fk0" FOREIGN KEY ("template_id") REFERENCES "template_story"("id");
+
+-- dummy data
--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,6 +1,6 @@
 /* the only line you likely need to change is
 
- database: 'prime_app',
+ database: 'journey_tellr',
 
  change `prime_app` to the name of your database, and you should be all set!
 */
@@ -30,7 +30,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'prime_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    database: 'journey_tellr', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
