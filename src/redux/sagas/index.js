2798bce695399087aeb6bbfd52dae0e9ef6b8561
Anthony D <dunahee@gmail.com>
2019-03-11 10:33:48 -0500

    contributor and story base sagas

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -3,6 +3,8 @@ import loginSaga from './loginSaga';
 import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import templateSaga from './templateSaga';
+import profileSaga from './profileSaga';
+import contributorSaga from './contributorSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -17,5 +19,7 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     templateSaga(),
+    profileSaga(),
+    contributorSaga(),
   ]);
 }
