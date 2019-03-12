5c27ef696ffdb1c1468897c3cb8ea5054f1ff462
Matt Kleven <klevennb115@gmail.com>
2019-03-12 14:34:07 -0500

    removed comments

--- src/components/Story/ExistingStory/ExistingStory.js
+++ src/components/Story/ExistingStory/ExistingStory.js
@@ -5,7 +5,7 @@ import ExistingStoryChapter from '../ExistingStory/ExistingStoryChapter';
 
 class ExistingStory extends Component {
     componentDidMount(){
-        this.props.dispatch  //get story and chapters here
+        // this.props.dispatch  //get story and chapters here
     }
 
     renderChapter = () => {
@@ -16,16 +16,16 @@ class ExistingStory extends Component {
     render() {
         return (
             <div>
-                <h1>Existing Story</h1>
+                {/* <h1>Existing Story</h1>
                 <div>
-                    <h1>Title: {this.props.story.title}</h1> {/*Title: to be deleted after routes are created */}
+                    <h1>Title: {this.props.story.title}</h1>
                 </div>
                 <div>
                     <h1>Photo: <img src={this.props.story.header_photo} width="100%" height="90" alt="Shows what caption describes"/></h1>
                     <h3>Caption: {this.props.story.caption} </h3>
                 </div>
-                <Divider />  {/*Ant Design thing. Will probably make chapters more distinct */}
-                {this.renderChapter()}  {/*Will probably have to add a delay to make sure everything is recieved from reducers. */}
+                <Divider />
+                {this.renderChapter()}   */}
             </div>
         )
     }
