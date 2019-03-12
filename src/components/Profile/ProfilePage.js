ecb18fc673ba0a4eca95c6a900c7b17b890357e3
James Tucker <jamesctucker@protonmail.com>
2019-03-12 14:58:02 -0500

    added profile picture, username, and edit buttons to the profile page

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
-import ImageUpload from '../ImageUpload/ImageUpload';
-const axios = require('axios');
+import { connect } from 'react-redux';
+import { Row, Col } from 'antd';
+import { Icon } from 'antd';
+import { Typography } from 'antd';
+import './ProfilePage.css';
+import 'antd/dist/antd.css';
+
+// import ImageUpload from '../ImageUpload/ImageUpload';
+// const axios = require('axios');
+
+const { Title } = Typography;
+const { Text } = Typography;
+
+
 
 class ProfilePage extends Component {
 
-    //posts to fakes.router.js
-    addFakePeople = (event) => {
-        axios.post('/fakes');
-    }
 
-    //posts to fakes.router.js
-    addFakeStories = (event) => {
-        axios.post('/fakes/story');
-    }
 
     render() {
 
         return (
             <div>
-                <div>
-                    <button onClick={this.addFakePeople}>Add 300 people to the database</button><br />
-                    <button onClick={this.addFakeStories}>Add 30 stories to the database</button>
-                    <ImageUpload />
-                </div>
+                <Row>
+                    <Col span={6}><img className="profile-element" src={this.props.reduxStore.user.profile_pic} height="75" /></Col>
+                    <Col span={10}><Title className="profile-element" level={4}>{this.props.reduxStore.user.first_name}&nbsp;{this.props.reduxStore.user.last_name}</Title></Col>
+                    <Col span={6}><Icon className="profile-element" type="edit" style={{ fontSize: '24px' }} /></Col>
+                </Row>
+                <Row>
+                    <Col span={6}><Text className="edit-btn">Edit</Text></Col>
+                    <Col span={10}></Col>
+                    <Col span={6}></Col>
+                </Row>
             </div>
         )
     }
 
 }
-export default ProfilePage;
+const mapStateToProps = reduxStore => ({
+    reduxStore
+});
+
+export default connect(mapStateToProps)(ProfilePage);
+// //posts to fakes.router.js
+// addFakePeople = (event) => {
+//     axios.post('/fakes');
+// }
+
+// //posts to fakes.router.js
+// addFakeStories = (event) => {
+//     axios.post('/fakes/story');
+// }
+
+{/* <div>
+                    <button onClick={this.addFakePeople}>Add 300 people to the database</button><br />
+                    <button onClick={this.addFakeStories}>Add 30 stories to the database</button>
+                    <ImageUpload />
+                </div> */}
