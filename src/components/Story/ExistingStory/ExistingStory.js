80c7af4ad0e85e489213a7b26bfa5ab9301bc1dd
Anthony D <dunahee@gmail.com>
2019-03-13 16:47:03 -0500

    chapter details available in existing story component from db

--- src/components/Story/ExistingStory/ExistingStory.js
+++ src/components/Story/ExistingStory/ExistingStory.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import { Divider } from 'antd';
+
 import ExistingStoryChapter from '../ExistingStory/ExistingStoryChapter';
 
 class ExistingStory extends Component {
 
-    componentDidMount(){
-        this.props.dispatch({type:'GET_INDIVIDUAL_STORY',
-                             payload: this.props.match.params.id});
+    componentDidMount() {
+        const { params } = this.props.match
+        this.props.dispatch({
+            type: 'GET_INDIVIDUAL_STORY',
+            payload: params.id
+        });
+        this.props.dispatch({
+            type: 'GET_STORY_CHAPTER_DETAIL',
+            payload: params.id
+        });
 
-                
+
     }
 
     handlePostStory = () => {
@@ -21,8 +30,10 @@ class ExistingStory extends Component {
     }
 
     handleGetContributors = () => {
-        this.props.dispatch({type: '', 
-                             payload: ''})
+        this.props.dispatch({
+            type: '',
+            payload: ''
+        })
     }
 
     // renderChapter = () => {
@@ -32,45 +43,41 @@ class ExistingStory extends Component {
     // }
 
     render() {
-        const { contributedStoryReducer,
-            summary,
-            likes,
-            contributor,
-            chapter, } = this.props.storyDetail
+        const { summary, likes, contributor,
+            chapter } = this.props.storyDetail
 
         return (
             <div>
                 {/* this will check that the storyDetail reducer is populated 
                 before rendering its contents */}
-                {summary.length !== 0 ? 
-                <div>
-                    <h1>Title: {summary[0].title}</h1>
-                    <h3>Photo: <img src={summary[0].header_photo}
-                                width='150px' 
-                                height='100px' 
-                                alt="Shows what caption describes"/></h3>
-                    <h3>Caption: {summary[0].caption}</h3>
+                {summary.length !== 0 ?
+                    <div>
+                        <h1>Title: {summary[0].title}</h1>
+                        <h3>Photo: <img src={summary[0].header_photo}
+                            width='150px'
+                            height='100px'
+                            alt="Shows what caption describes" /></h3>
+                        <h3>Caption: {summary[0].caption}</h3>
 
-                    <a onClick={this.handleGetContributors}><u>Contributors: </u></a><br/>
-                    <button onClick={this.handleAddChapter}>Add Chapter</button><br/>
-                    <button onClick={this.handlePostStory}>Post Story</button>
-                </div> : null 
-                // when the component mounts
+                        <a onClick={this.handleGetContributors}><u>Contributors: </u></a><br />
+                        <button onClick={this.handleAddChapter}>Add Chapter</button><br />
+                        <button onClick={this.handlePostStory}>Post Story</button>
+                    </div> : null
+                    // when the component mounts
 
                 }
                 {/* chapters div here */}
-
+                <ExistingStoryChapter chapter={chapter} />
                 {/* contributor button here */}
 
                 {/* post story button here only if author of story */}
-                
+
             </div>
         )
     }
 };
 const mapStoreToProps = reduxStore => ({
     storyDetail: reduxStore.storyDetail,
-    chapter: reduxStore.chapter,
 })
 
 export default connect(mapStoreToProps)(ExistingStory);
--- src/redux/sagas/storyDetailSaga.js
+++ src/redux/sagas/storyDetailSaga.js
@@ -5,15 +5,21 @@ function* getIndividualStory(action) {
     try {
         console.log('in getIndividualStory saga, action.payload: ', action.payload);
         const serverResponse = yield axios.get(`story/detail/${action.payload}`);
-
         yield put({ type: 'SET_STORY_DETAIL', payload: serverResponse.data });
-
     } catch (error) {
         console.log(`Error in getting individual story: ${error}`);
     }
 }
-//chapers
 
+//chapers
+function* getChapterDetail(action) {
+    try {
+        const response = yield axios.get(`story/detail/chapter/${action.payload}`);
+        yield put({ type: 'SET_STORY_DETAIL_CHAPTER', payload: response.data });
+    } catch (error) {
+        console.log(`Error in getChapterDetail saga:`, error);
+    }
+}
 //contributors
 
 //likes
@@ -22,6 +28,7 @@ function* getIndividualStory(action) {
 
 function* storyDetailSaga() {
     yield takeLatest('GET_INDIVIDUAL_STORY', getIndividualStory);
+    yield takeLatest('GET_STORY_CHAPTER_DETAIL', getChapterDetail);
 }
 
 export default storyDetailSaga;
