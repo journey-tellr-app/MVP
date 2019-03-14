15d377d261da2982d09a38a2a6894dac68a9daf9
Kye Berkeland <kyeberkeland@yahoo.com>
2019-03-14 13:58:26 -0500

    Fix edit story chapter numbering

--- src/components/Story/NewStory/EditChapterPage.js
+++ src/components/Story/NewStory/EditChapterPage.js
@@ -4,31 +4,15 @@ import { connect } from 'react-redux';
 // ant design import
 import { Form, Input, Icon, Button, List } from 'antd';
 
-class NewStoryChapter extends Component {
+class EditChapterPage extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
             title: '',
-            itemTitle: '',
-            order: 0,
         }
     }
-
-    // removes a user created chapter
-    removeChapter = (chapterIn) => {
-        this.props.dispatch({ type: 'REMOVE_NEW_STORY_CHAPTER', payload: chapterIn });
-    } // end removeChapter
-
-    // allow for editing a chapter title
-    editChapter = (chapterIn) => {
-        let toggle = !chapterIn.disabled;
-        chapterIn.disabled = toggle;
-        console.log(chapterIn.itemTitle);
-        chapterIn.title = this.state.itemTitle != ' ' ? this.state.itemTitle : chapterIn.itemTitle;
-        this.props.dispatch({ type: 'UPDATE_NEW_STORY_CHAPTER', payload: chapterIn });
-    } // end editChapter
 
     // function for setting local state with user input
     onInputChange = (event) => {
@@ -40,17 +24,23 @@ class NewStoryChapter extends Component {
 
     // submits the currently entered field and allows the user to create another chapter
     addChapter = () => {
+        // add 1 to the length of existing chapters to get the new chapter order
+        let newOrder = this.props.chapter.length + 1;
+
+        // create an array to use the existing chapter router POST route
         let chapterArray = [];
-        chapterArray.push({ title: this.state.title, order: this.state.order, disabled: true, });
+        chapterArray.push({ title: this.state.title, order: newOrder, disabled: true, });
+
+        // package the data to be sent to the server
         let dataToSend = { chapter: chapterArray,
                            storyId: this.props.storyId,
                          };
-
+        // call reduxSaga for adding a new chapter passing the chapter array and story id
         this.props.dispatch({ type: 'ADD_EXISTING_STORY_CHAPTER', payload: dataToSend });
-        let newOrder = this.state.order += 1;
+
+        // clear state - not quite working properly
         this.setState({
             title: ' ',
-            order: newOrder,
         });
     }
 
@@ -71,7 +61,7 @@ class NewStoryChapter extends Component {
 };
 
 const mapStoreToProps = reduxStore => ({
-    chapter: reduxStore.chapter.newStoryChapterReducer,
+    chapter: reduxStore.storyDetail.chapter,
 });
 
-export default connect(mapStoreToProps)(NewStoryChapter);
+export default connect(mapStoreToProps)(EditChapterPage);
