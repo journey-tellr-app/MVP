4d77deea040fc662b59f193aeca2fe59d58df620
Anthony D <dunahee@gmail.com>
2019-03-15 15:33:40 -0500

    edit button dispatches to server if text field is edited

--- src/components/Story/ExistingStory/ChapterView/ChapterEditButton.js
+++ src/components/Story/ExistingStory/ChapterView/ChapterEditButton.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
 
-import { Modal, Button, Input } from 'antd';
+import { Modal, Button, Input, Icon } from 'antd';
 
 class ChapterEditButton extends Component {
     state = {
@@ -13,29 +13,47 @@ class ChapterEditButton extends Component {
     static propTypes = {
         valueToEdit: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
     }
 
-    handleModalBtn = (value) => {
+    handleModalBtn = (value, e) => {
         this.setState({ modalVisible: value, })
     }
 
     handleEdit = (e) => {
-        console.log(e)
+        // console.log(e)
         this.setState({ editedValue: e.target.value })
     }
 
-    render() {
-        console.log(this.props);
+    handleSubmit = () => {
+        // console.log('in submit');
         const { editedValue } = this.state;
-        const { valueToEdit, name, type } = this.props;
+        let { name, id } = this.props;
+        if(editedValue !== null){
+            this.props.dispatch({
+                type: 'EDIT_CHAPTER',
+                payload: {
+                    id: id,
+                    colName: name,
+                    updatedValue: editedValue,
+                },
+            })
+        }
+    }
+
+    render() {
+        // console.log(this.props);
+        const { editedValue } = this.state;
+        const { valueToEdit, name, type, action } = this.props;
         return (
             <div>
-                <Button type="primary" onClick={() => this.handleModalBtn(true)}>{`Edit ${name}`} </Button>
+                <Button type="default" icon='edit' onClick={this.handleModalBtn.bind(this, true)}>{`${name}`} </Button>
                 <Modal
                     title={`Editing ${type} ${name}`}
                     style={{ top: 20 }}
                     visible={this.state.modalVisible}
-                    onOk={() => this.handleModalBtn(false)}
+                    onOk={this.handleSubmit}
                     onCancel={() => this.handleModalBtn(false)}
                 >
                     <Input.TextArea value={editedValue === null ? valueToEdit : editedValue} onChange={this.handleEdit} />
