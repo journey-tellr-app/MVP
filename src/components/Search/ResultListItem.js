698799f174f7a66ffdfaab1e8584bc4f3fa0bfa9
Matt Kleven <klevennb115@gmail.com>
2019-03-18 16:52:08 -0500

    search results card working

--- src/components/Search/ResultListItem.js
+++ src/components/Search/ResultListItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 
-import {List, Icon, Avatar } from 'antd';
+import { List, Icon, Avatar, Card, Row, Col } from 'antd';
 
 class ResultListItem extends Component {
     static propTypes = {
@@ -13,22 +13,40 @@ class ResultListItem extends Component {
 
   render() {
       const { item } = this.props;
+      const { Meta } = Card;
       console.log(item);
     return (
-      <div>
-            <List.Item
-                key={item.title}
-                actions={[<Icon type="star-o" text={item.likes} />, <Icon type="like-o" text="156" />, <Icon type="message" text="2" />]}
-                extra={<img width={272} alt="logo" src={item.header_photo} />}
-            >
-                <List.Item.Meta
-                    avatar={<Avatar src={item.profile_pic} />}
-            title={<a href={`http://localhost:3000/#/existing-story/${item.story_id}`}>{item.title}</a>}
-                    description={item.description}
-                />
-                {item.content}
-                </List.Item>
-      </div>
+      <Row>
+        <Col span={12}>            
+        <List.Item
+        >
+          <List.Item.Meta
+            title={item.title}
+            description={item.intro}
+          />
+          {item.content}
+        </List.Item>
+          <List.Item
+            key={item.title}
+            actions={[<Icon type="star-o" text={item.likes} />, <Icon type="like-o" text="156" />, <Icon type="message" text="2" />]}
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={item.profile_pic} />}
+              description={item.full_name}
+            />
+          </List.Item>
+        </Col>
+        <Col span={12}><Card
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt="example" src={item.header_photo} />}
+        >
+          <Meta
+            description={item.caption}
+          />
+        </Card></Col>
+      </Row>
+
     )
   }
 }
