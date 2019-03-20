2bd3ca8fbfcaec1a1779de4fb7c8828c04b5d09c
Anthony D <dunahee@gmail.com>
2019-03-20 16:06:04 -0500

    subheader displays user location on page, requires text of subheader

--- src/components/Common/SubHeader.js
+++ src/components/Common/SubHeader.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Row, Col, Typography } from 'antd';
+import './SubHeader.css';
+
+const { Title } = Typography;
 
 export default class SubHeader extends Component {
     static propTypes = {
         headerText: PropTypes.string.isRequired,
     }
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <Row type="flex"
+                justify="space-around"
+                align="middle"
+                gutter={16}
+                className='sub-header'>
+                <Title 
+                    level={2} 
+                    className='sub-header-text'>
+                    {this.props.headerText}
+                </Title>
+            </Row>
+        )
+    }
 }
