008f4b6331733ce15cf1ab52b31701d810e94def
Britt Carter <brittbmh@gmail.com>
2019-05-10 15:04:04 -0500

    updated typo in database file

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -92,7 +92,8 @@ class ProfilePage extends Component {
 
 
     render() {
-
+        console.log(this.props.story);
+        
         return (
 
             <div className="container" >
@@ -111,17 +112,20 @@ class ProfilePage extends Component {
                     <div className="stats-div">
                         <Row gutter={16}>
                             <Col xs={24}>
-                                <Icon className="profile-element" type="calendar" style={{ fontSize: "14px" }} /><Text className="stats-text">Member since&nbsp;{moment(this.props.user.userInfo.date_created).format("MMM Do, YYYY")}</Text>
+                                <Icon className="profile-element" type="calendar" style={{ fontSize: "14px" }} />
+                                <Text className="stats-text">Member since&nbsp;{moment(this.props.user.userInfo.date_created).format("MMM Do, YYYY")}</Text>
                             </Col>
                         </Row>
                         <Row gutter={16}>
                             <Col xs={24}>
-                                <Icon className="profile-element" type="book" style={{ fontSize: "14px" }} /><Text className="stats-text">{this.props.story.userStoryReducer.length}&nbsp;Stories</Text>
+                                <Icon className="profile-element" type="book" style={{ fontSize: "14px" }} />
+                                <Text className="stats-text">{this.props.story.userStoryReducer.length}&nbsp;Stories</Text>
                             </Col>
                         </Row>
                         <Row gutter={16}>
                             <Col xs={24}>
-                                <Icon className="profile-element" type="profile" style={{ fontSize: "14px" }} /><Text className="stats-text">{this.props.story.contributedStoryReducer.length}&nbsp;Contributions</Text>
+                                <Icon className="profile-element" type="profile" style={{ fontSize: "14px" }} />
+                                <Text className="stats-text">{this.props.story.contributedStoryReducer.length}&nbsp;Contributions</Text>
                             </Col>
                         </Row>
                     </div>
